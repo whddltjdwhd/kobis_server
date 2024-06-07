8475b5337c40cdf35ac47712fb9d83b437cca7ff
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-06-08 03:53:11 +0900

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
+import cors from "cors"; // cors 미들웨어를 가져옵니다.
 
 const app = express();
+app.use(cors()); // cors 미들웨어를 추가합니다.
 
 const users = [
   { id: 1, username: "user1" },
@@ -9,11 +11,12 @@ const users = [
   { id: 3, username: "user3" },
 ];
 
+// '/users' 경로로 사용자 데이터를 제공합니다.
 app.get("/user", (req: Request, res: Response, next: NextFunction) => {
   res.json(users);
 });
 
-app.use("/user", express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 const PORT = 8000;
 
