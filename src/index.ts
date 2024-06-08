589d40b36c3957f9bf4c5c9653f3af319988194f
Rune <sungjong5k@naver.com>
2024-06-08 16:24:33 +0900

    Remove useless variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
 import cors from "cors"; // cors 미들웨어를 가져옵니다.
+const PORT = 8000;
 
 const bodyParser = require("body-parser");
 const app = express();
@@ -14,26 +15,19 @@ app.use(cors()); // cors 미들웨어를 추가합니다.
 app.set("port", process.env.PORT || 8000); // 포트 설정
 app.set("host", process.env.HOST || "0.0.0.0"); // 아이피 설정
 
-const users = [
-  { id: 1, username: "user1" },
-  { id: 2, username: "user2" },
-  { id: 3, username: "user3" },
-];
-
 app.get("/movies", (req: Request, res: Response, next: NextFunction) => {
-  const query = "select id, title, year from movie limit 10";
+  const query = "select id, title, year from movie";
   conn.query(query, function (err: any, result: any) {
     if (err) {
       console.log("query is not excuted: " + err);
-    } else {
-      res.send(result);
+      return;
     }
+
+    res.send(result);
   });
 });
 
 app.use(express.static(path.join(__dirname, "./client/build")));
-
-const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`
