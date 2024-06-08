3e60f72fb4f01adc77f7bb997d81a18535ad930c
Rune <sungjong5k@naver.com>
2024-06-08 14:41:32 +0900

    Connect with Moives table

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import express, { Request, Response, NextFunction } from "express";
 import path from "path";
 import cors from "cors"; // cors 미들웨어를 가져옵니다.
 
+const bodyParser = require("body-parser");
 const app = express();
+const db = require("./mysql.ts");
+const conn = db.init();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors()); // cors 미들웨어를 추가합니다.
+
+app.set("port", process.env.PORT || 8000); // 포트 설정
+app.set("host", process.env.HOST || "0.0.0.0"); // 아이피 설정
 
 const users = [
   { id: 1, username: "user1" },
@@ -11,9 +20,15 @@ const users = [
   { id: 3, username: "user3" },
 ];
 
-// '/users' 경로로 사용자 데이터를 제공합니다.
-app.get("/user", (req: Request, res: Response, next: NextFunction) => {
-  res.json(users);
+app.get("/movies", (req: Request, res: Response, next: NextFunction) => {
+  const query = "select id, title, year from movie limit 10";
+  conn.query(query, function (err: any, result: any) {
+    if (err) {
+      console.log("query is not excuted: " + err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.use(express.static(path.join(__dirname, "./client/build")));
